1024504640c4f3cd764c4d5ef13adb73ee53f6b0
Yuri <yuri@gudideas.co.uk>
2025-10-16 11:35:11 +0100

    Update theme

--- dtm/js/custom.js
+++ dtm/js/custom.js
@@ -1,28 +1,28 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 	// Scroll To Top Toggle
-	$(window).scroll(function(){
-		if($(this).scrollTop() > 300){
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
 			$("#move-up-toggle").fadeIn();
-		}
-		else{
+		} else {
 			$("#move-up-toggle").fadeOut();
 		}
 	});
-	$("#move-up-toggle").click(function(){
-		$("html, body").animate({ scrollTop: 0 }, "slow");
-  		return false;
+	$("#move-up-toggle").click(function () {
+		$("html, body").animate({
+			scrollTop: 0
+		}, "slow");
+		return false;
 	});
 	// Mobile Menu - Menu Toggle
-	$('.menu-toggle').click(function(){
-		if($('.primary-navigation').is(':hidden')){
+	$('.menu-toggle').click(function () {
+		if ($('.primary-navigation').is(':hidden')) {
 			$('.primary-navigation').fadeIn();
 			$(this).children('i').removeClass('fa-bars');
 			$(this).children('i').addClass('fa-times');
 			$('body').css({
 				overflow: 'hidden'
 			});
-		}
-		else{
+		} else {
 			$('.primary-navigation').fadeOut();
 			$(this).children('i').removeClass('fa-times');
 			$(this).children('i').addClass('fa-bars');
@@ -32,131 +32,178 @@ jQuery(document).ready(function($){
 		}
 	});
 	// Mobile Menu - Sub-menu Toggle
-	$(".primary-navigation ul li.menu-item-has-children > .arrow").click(function(){
-		if(!$(this).parent().children("ul.sub-menu").hasClass('drop')){
+	$(".primary-navigation ul li.menu-item-has-children > .arrow").click(function () {
+		if (!$(this).parent().children("ul.sub-menu").hasClass('drop')) {
 			$(this).parent().children("ul.sub-menu").addClass('drop');
 			$(this).parent().children("ul.sub-menu").slideDown();
 			$(this).children("i").removeClass('fa-angle-down');
 			$(this).children("i").addClass('fa-angle-up');
-		}
-		else{
+		} else {
 			$(this).parent().children("ul.sub-menu").removeClass('drop');
 			$(this).parent().children("ul.sub-menu").slideUp();
 			$(this).children("i").removeClass('fa-angle-up');
 			$(this).children("i").addClass('fa-angle-down');
 		}
 	});
-	if($(window).width() > 767){
+	if ($(window).width() > 767) {
 		// WOW
 		new WOW().init();
 	}
-	if($(window).width() > 991){
+	if ($(window).width() > 991) {
 		// Paroller
 		$('.paroller').paroller();
 	}
 	/* Banner */
 	$(".banner .banner-services ul li").hover(
-		function(){
-			$('.icon-box', this).not('.in .icon-box').stop(true,true).addClass('animated shake');
+		function () {
+			$('.icon-box', this).not('.in .icon-box').stop(true, true).addClass('animated shake');
 		},
-		function(){
-			$('.icon-box', this).not('.in .icon-box').stop(true,true).removeClass('animated shake');
+		function () {
+			$('.icon-box', this).not('.in .icon-box').stop(true, true).removeClass('animated shake');
 		}
 	);
 	// Scroll
-	$('.scroll-button-toggle').click(function(){
-		$("html, body").animate({ scrollTop: $('#scroll-target').offset().top }, "slow");
-  		return false;
+	$('.scroll-button-toggle').click(function () {
+		$("html, body").animate({
+			scrollTop: $('#scroll-target').offset().top
+		}, "slow");
+		return false;
 	});
 	// Banner Scroll Down
-	$('.banner .scroll-down a').click(function(){
-		$("html, body").animate({ scrollTop: $('#scroll-to').offset().top }, "slow");
-  		return false;
+	$('.banner .scroll-down a').click(function () {
+		$("html, body").animate({
+			scrollTop: $('#scroll-to').offset().top
+		}, "slow");
+		return false;
 	});
 	// Count to Number
 	$('.count').each(function () {
-	  	var $this = $(this);
-	  	jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-		    duration: 1500,
-		    easing: 'swing',
-		    step: function () {
-		      $this.text(Math.ceil(this.Counter));
-		    }
-	 	});
+		var $this = $(this);
+		jQuery({
+			Counter: 0
+		}).animate({
+			Counter: $this.text()
+		}, {
+			duration: 1500,
+			easing: 'swing',
+			step: function () {
+				$this.text(Math.ceil(this.Counter));
+			}
+		});
 	});
 	// Privacy Policy
-	jQuery(".privacy-policy .pp-list li a").click(function(e){
+	jQuery(".privacy-policy .pp-list li a").click(function (e) {
 		var scrollto = $(this).attr('href');
-		$("html, body").animate({ scrollTop: $(scrollto).offset().top - 30 }, "slow");
+		$("html, body").animate({
+			scrollTop: $(scrollto).offset().top - 30
+		}, "slow");
 		e.preventDefault();
 	});
 	// Resource Loader
-	jQuery(".resource-loader").click(function(){
+	jQuery(".resource-loader").click(function () {
 		source = jQuery(this).attr('load-source');
 		source_tag = jQuery(this).attr('source-tag');
 
 		jQuery(this).addClass('active');
 
-		if(source_tag == 'iframe'){
+		if (source_tag == 'iframe') {
 			jQuery(this).children('.resource-content').html('<iframe src="' + source + '?autoplay=1" width="640" height="360"></iframe>');
 		}
 	});
 	/* Packages */
-	jQuery(".package-box").hover(function(e){
+	jQuery(".package-box").hover(function (e) {
 		jQuery('.package-box.active').removeClass('active');
 		jQuery(this).addClass('active');
 	});
-	  // FAQs
-  $(".faq-link .question-wrapper").click(function () {
-    $(".faq-link.active .answer-wrapper").slideUp()
-    $(".faq-link.active").removeClass("active")
+	// FAQs
+	$(".faq-link .question-wrapper").click(function () {
+		$(".faq-link.active .answer-wrapper").slideUp()
+		$(".faq-link.active").removeClass("active")
 
-    if (
-      $(this).parents(".faq-link").children(".answer-wrapper").is(":hidden")
-    ) {
-      $(this).parents(".faq-link").addClass("active")
-      $(this).parents(".faq-link").children(".answer-wrapper").slideDown()
-    } else {
-      $(this).parents(".faq-link").removeClass("active")
-      $(this).parents(".faq-link").children(".answer-wrapper").slideUp()
-    }
+		if (
+			$(this).parents(".faq-link").children(".answer-wrapper").is(":hidden")
+		) {
+			$(this).parents(".faq-link").addClass("active")
+			$(this).parents(".faq-link").children(".answer-wrapper").slideDown()
+		} else {
+			$(this).parents(".faq-link").removeClass("active")
+			$(this).parents(".faq-link").children(".answer-wrapper").slideUp()
+		}
 
-    return false
-  })
+		return false
+	})
 
-  $(".pricetable-box-heading").click(function () {
-    if (
-      $(this).parents(".pricetable-box").children(".pricetable-box-content").is(":hidden")
-    ) {
-      $(this).parents(".pricetable-box").addClass("active")
-      $(this).parents(".pricetable-box").children(".pricetable-box-content").slideDown()
-    } else {
-      $(this).parents(".pricetable-box").removeClass("active")
-      $(this).parents(".pricetable-box").children(".pricetable-box-content").slideUp()
-    }
-
-    return false
-  })
+	//   Packages
+	$(".pricetable-box-heading").click(function () {
+		if (
+			$(this).parents(".pricetable-box").children(".pricetable-box-content").is(":hidden")
+		) {
+			$(this).parents(".pricetable-box").addClass("active")
+			$(this).parents(".pricetable-box").children(".pricetable-box-content").slideDown()
+		} else {
+			$(this).parents(".pricetable-box").removeClass("active")
+			$(this).parents(".pricetable-box").children(".pricetable-box-content").slideUp()
+		}
+		return false
+	})
 
   const priceToggle = document.getElementById('price-toggle');
-  const prices = document.querySelectorAll('.price');
-  const priceLabels = document.querySelectorAll('.price-label');
+  if (priceToggle) {
+    const prices = document.querySelectorAll('.price');
+    const priceLabels = document.querySelectorAll('.price-label');
 
-  priceToggle.addEventListener('change', function() {
-    if (this.checked) {
-      prices.forEach(price => {
-        price.innerHTML = `${price.dataset.annually}<span class="price-label">/ann</span> <span class="vat">(+VAT)</span>`;
-      });
-    //   priceLabels.forEach(label => {
-    //     label.textContent = '/ann';
-    //   });
-    } else {
-      prices.forEach(price => {
-        price.innerHTML = `${price.dataset.monthly}<span class="price-label">/mo</span> <span class="vat">(+VAT)</span>`;
-      });
-    //   priceLabels.forEach(label => {
-    //     label.textContent = '/mo';
-    //   });
-    }
-  });
+    priceToggle.addEventListener('change', function() {
+      if (this.checked) {
+        prices.forEach(price => {
+          price.innerHTML = `${price.dataset.annually} <span class="vat">(+VAT)</span>`;
+        });
+        priceLabels.forEach(label => {
+          label.textContent = '/annually';
+        });
+      } else {
+        prices.forEach(price => {
+          price.innerHTML = `${price.dataset.monthly} <span class="vat">(+VAT)</span>`;
+        });
+        priceLabels.forEach(label => {
+          label.textContent = '/monthly';
+        });
+      }
+    });
+  }
+	//   END Packages
+
+	//   CF7 Multistep Form
+	function initCf7MultiStepForm() {
+		const form = document.querySelector('.multistep-form');
+		if (!form || form.classList.contains('multistep-initialized')) return;
+
+		form.classList.add('multistep-initialized');
+
+		const fieldsets = form.querySelectorAll('fieldset');
+		const nextBtns = form.querySelectorAll('.next');
+		const prevBtns = form.querySelectorAll('.previous');
+		const progressBar = form.querySelectorAll('#progressbar li');
+		let currentStep = 0;
+
+		function showStep(n) {
+			fieldsets.forEach((f, i) => f.style.display = i === n ? 'block' : 'none');
+			progressBar.forEach((p, i) => p.classList.toggle('active', i <= n));
+		}
+		nextBtns.forEach(btn => btn.addEventListener('click', () => {
+			if (currentStep < fieldsets.length - 1) currentStep++;
+			showStep(currentStep);
+		}));
+		prevBtns.forEach(btn => btn.addEventListener('click', () => {
+			if (currentStep > 0) currentStep--;
+			showStep(currentStep);
+		}));
+		showStep(currentStep);
+	}
+
+	document.addEventListener('wpcf7spam', function (event) {
+		initCf7MultiStepForm();
+	}, false);
+
+	initCf7MultiStepForm(); 
+//   END CF7 Multistep Form
 });
